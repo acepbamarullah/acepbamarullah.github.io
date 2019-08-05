32ec78c3143073f07434bfd111507226db3f8727
maliki <nurmaliki58@gmail.com>
2019-08-05 16:20:19 +0700

    [UPDATE] Ubah data

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <header>
-      <h1>PokeMan</h1>
+      <h1><img src="/assets/pokeball.png" width="5%" height="5%" style="vertical-align: middle;"/> PokeMan</h1>
     </header>
     <main>
 
@@ -11,6 +11,7 @@
     <router-link
         v-for="pokemon in pokemons"
         active-class="is-active"
+        v-bind:key="pokemon.name"
         class="link"
         :to="{ name: 'PokemonDetail', params: { name: pokemon.name } }">
       <li>{{ pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1) }}</li>
@@ -24,10 +25,31 @@
   </div>
 </div>
 
+<div id="pocket">
+    <img src="./assets/pocket.png" />
+</div>
+
     </main>
   </div>
 </template>
 <style>
+@keyframes shake {
+  10%, 90% {
+    transform: translate3d(-1px, 0, 0);
+  }
+
+  20%, 80% {
+    transform: translate3d(2px, 0, 0);
+  }
+
+  30%, 50%, 70% {
+    transform: translate3d(-4px, 0, 0);
+  }
+
+  40%, 60% {
+    transform: translate3d(4px, 0, 0);
+  }
+}
 * {
   box-sizing: border-box;
 }
@@ -60,6 +82,21 @@
   height: 100%; /* Should be removed. Only for demonstration */
 }
 
+#pocket img {
+position: fixed;
+right: 0px;
+bottom: 0%;
+}
+
+#pocket-shake img {
+position: fixed;
+right: 0px;
+bottom: 0%;
+animation: shake 1s infinite;
+-webkit-animation: shake 1s infinite;
+-moz-animation: shake 1s infinite;
+-o-animation: shake 1s infinite;
+}
 /* Clear floats after the columns */
 .row:after {
   content: "";
