d6cdbbcc9af8bf6e9b5f9bcbbf103476092ca21a
maliki <nurmaliki58@gmail.com>
2019-08-07 16:14:12 +0700

    [UPDATE] Ubah data

--- src/App.vue
+++ src/App.vue
@@ -26,21 +26,7 @@
           </div>
         </div>
       </div>
-
-      <div id="pocket">
-        <a href="#pocketmonster" @click="setToTop">
-        <img src="assets/pocket.png" />
-        </a>
-      </div>
     </main>
-    <div class="modal" id="pocketmonster">
-      <div class="modal-container">
-        <h2><img src="assets/pocketicon.png" style="position: relative; vertical-align: middle;"/>
-        Your Pokemon Pocket</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac iaculis est. Proin a nisl sit amet turpis facilisis euismod. Praesent sit amet nisl ornare, volutpat orci at, pretium ipsum. Sed sed ante a ex vulputate iaculis sed et mauris. Suspendisse potenti. In hac habitasse platea dictumst.</p>
-        <a href="#modal-close">Close</a>
-      </div>
-    </div>
   </div>
 </template>
 <style>
@@ -65,32 +51,6 @@ body {
    transform: translateY(-10px);
  }
 },
-@keyframes shake {
-  10%,
-  90% {
-    transform: translate3d(-1px, 0, 0);
-  }
-
-  20%,
-  80% {
-    transform: translate3d(2px, 0, 0);
-  }
-
-  30%,
-  50%,
-  70% {
-    transform: translate3d(-4px, 0, 0);
-  }
-
-  40%,
-  60% {
-    transform: translate3d(4px, 0, 0);
-  }
-}
-* {
-  box-sizing: border-box;
-}
-
 /* Create two equal columns that floats next to each other */
 .column {
   float: left;
@@ -144,46 +104,6 @@ body {
   display: table;
   clear: both;
 }
-p {
-  margin-top: 0;
-}
-.modal-container {
-  position: fixed;
-  background-color: #fff;
-  width: 70%;
-  max-width: 80%;
-  height: 80%;
-  left: 50%;
-  padding: 20px;
-  border-radius: 5px;
-  -webkit-transform: translate(-50%, -200%);
-  -ms-transform: translate(-50%, -200%);
-  transform: translate(-50%, -200%);
-  -webkit-transition: -webkit-transform 200ms ease-out;
-  transition: transform 200ms ease-out;
-}
-.modal:before {
-  content: "";
-  position: fixed;
-  display: none;
-  background-color: rgba(0, 0, 0, 0.8);
-  top: 0;
-  left: 0;
-  height: 100%;
-  width: 100%;
-}
-.modal:target:before {
-  display: block;
-}
-.modal:target .modal-container {
-  top: 5%;
-  -webkit-transform: translate(-50%, 0%);
-  -ms-transform: translate(-50%, 0%);
-  transform: translate(-50%, 0%);
-}
-#modal-close {
-
-}
 </style>
 <script>
 import axios from "axios";
@@ -191,6 +111,7 @@ export default {
   data() {
     return {
       pokemons: null,
+      mypokemondetail: [],
       endpoint:
         "https://pokeapi.co/api/v2/pokemon?offset=0&limit=10000000000000000"
     };
@@ -212,13 +133,6 @@ export default {
           console.log("-----error-------");
           console.log(error);
         });
-    },
-    setToTop() {
-      // alert('tes')
-      setTimeout(function(){
-      document.body.scrollTop = 0; // For Safari
-      document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-      }, 100);
     }
   }
 };
