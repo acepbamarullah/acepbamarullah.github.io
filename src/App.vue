57db55777e73c7384fbec7fd01eb848fb642de64
maliki <nurmaliki58@gmail.com>
2019-08-07 14:03:14 +0700

    [UPDATE] Tambah modal

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,89 @@
 <template>
   <div id="app">
     <header>
-      <h1><img src="assets/pokeball.png" width="5%" height="5%" style="vertical-align: middle;"/> PokeMan</h1>
+      <h1>
+        <img src="assets/pokeball.png" width="5%" height="5%" style="vertical-align: middle;" /> PokeMan
+      </h1>
     </header>
     <main>
-
-<div class="row">
-  <div class="column-left" style="background-color:#aaa;">
- <ul>
-    <router-link
-        v-for="pokemon in pokemons"
-        active-class="is-active"
-        v-bind:key="pokemon.name"
-        class="link"
-        :to="{ name: 'PokemonDetail', params: { name: pokemon.name } }">
-      <li>{{ pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1) }}</li>
-    </router-link>
-    </ul>
-  </div>
-  <div class="column-right" style="background-color:#bbb;">
-   <div class="content">
-        <router-view></router-view>
+      <div class="row">
+        <div class="column-left" style="background-color:#aaa;opacity: 1;">
+          <ul>
+            <router-link
+              v-for="pokemon in pokemons"
+              active-class="is-active"
+              v-bind:key="pokemon.name"
+              class="link"
+              :to="{ name: 'PokemonDetail', params: { name: pokemon.name } }"
+            >
+              <li>{{ pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1) }}</li>
+            </router-link>
+          </ul>
+        </div>
+        <div class="column-right" style="background-color:#bbb;opacity: 0.5;">
+          <div class="content">
+            <router-view></router-view>
+          </div>
+        </div>
       </div>
-  </div>
-</div>
 
-<div id="pocket">
-    <img src="assets/pocket.png" />
-</div>
-
+      <div id="pocket">
+        <a href="#pocketmonster" @click="setToTop">
+        <img src="assets/pocket.png" />
+        </a>
+      </div>
     </main>
+    <div class="modal" id="pocketmonster">
+      <div class="modal-container">
+        <h2><img src="assets/pocketicon.png" style="position: relative; vertical-align: middle;"/>
+        Your Pokemon Pocket</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac iaculis est. Proin a nisl sit amet turpis facilisis euismod. Praesent sit amet nisl ornare, volutpat orci at, pretium ipsum. Sed sed ante a ex vulputate iaculis sed et mauris. Suspendisse potenti. In hac habitasse platea dictumst.</p>
+        <a href="#modal-close">Close</a>
+      </div>
+    </div>
   </div>
 </template>
 <style>
+body {
+  background-color: #272727;
+  background-image: url("assets/background.jpg");
+  background-position: bottom right;
+  background-repeat:no-repeat;
+  background-attachment: fixed;
+},
+@keyframes bounce {
+ 0%, 20%, 60%, 100% {
+   -webkit-transform: translateY(0);
+   transform: translateY(0);
+ }
+ 40% {
+   -webkit-transform: translateY(-20px);
+   transform: translateY(-20px);
+ }
+ 80% {
+   -webkit-transform: translateY(-10px);
+   transform: translateY(-10px);
+ }
+},
 @keyframes shake {
-  10%, 90% {
+  10%,
+  90% {
     transform: translate3d(-1px, 0, 0);
   }
 
-  20%, 80% {
+  20%,
+  80% {
     transform: translate3d(2px, 0, 0);
   }
 
-  30%, 50%, 70% {
+  30%,
+  50%,
+  70% {
     transform: translate3d(-4px, 0, 0);
   }
 
-  40%, 60% {
+  40%,
+  60% {
     transform: translate3d(4px, 0, 0);
   }
 }
@@ -69,6 +106,7 @@
   overflow: scroll;
 }
 .column-right {
+  opacity: 0.5;
   float: left;
   width: 80%;
   padding: 10px;
@@ -83,19 +121,22 @@
 }
 
 #pocket img {
-position: fixed;
-right: 0px;
-bottom: 0%;
+  position: fixed;
+  right: 0px;
+  bottom: 0%;
 }
 
+.pocket-bounces {
+  animation: bounce 1s infinite;
+}
 #pocket-shake img {
-position: fixed;
-right: 0px;
-bottom: 0%;
-animation: shake 1s infinite;
--webkit-animation: shake 1s infinite;
--moz-animation: shake 1s infinite;
--o-animation: shake 1s infinite;
+  position: fixed;
+  right: 0px;
+  bottom: 0%;
+  animation: shake 1s infinite;
+  -webkit-animation: shake 1s infinite;
+  -moz-animation: shake 1s infinite;
+  -o-animation: shake 1s infinite;
 }
 /* Clear floats after the columns */
 .row:after {
@@ -103,15 +144,56 @@ animation: shake 1s infinite;
   display: table;
   clear: both;
 }
+p {
+  margin-top: 0;
+}
+.modal-container {
+  position: fixed;
+  background-color: #fff;
+  width: 70%;
+  max-width: 80%;
+  height: 80%;
+  left: 50%;
+  padding: 20px;
+  border-radius: 5px;
+  -webkit-transform: translate(-50%, -200%);
+  -ms-transform: translate(-50%, -200%);
+  transform: translate(-50%, -200%);
+  -webkit-transition: -webkit-transform 200ms ease-out;
+  transition: transform 200ms ease-out;
+}
+.modal:before {
+  content: "";
+  position: fixed;
+  display: none;
+  background-color: rgba(0, 0, 0, 0.8);
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+}
+.modal:target:before {
+  display: block;
+}
+.modal:target .modal-container {
+  top: 5%;
+  -webkit-transform: translate(-50%, 0%);
+  -ms-transform: translate(-50%, 0%);
+  transform: translate(-50%, 0%);
+}
+#modal-close {
+
+}
 </style>
 <script>
-import axios from 'axios'
+import axios from "axios";
 export default {
-  data () {
+  data() {
     return {
       pokemons: null,
-      endpoint: 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=10000000000000000',
-    }
+      endpoint:
+        "https://pokeapi.co/api/v2/pokemon?offset=0&limit=10000000000000000"
+    };
   },
 
   created() {
@@ -120,15 +202,24 @@ export default {
 
   methods: {
     getAllPosts() {
-      axios.get(this.endpoint)
+      this.pokemons =  null
+      axios
+        .get(this.endpoint)
         .then(response => {
           this.pokemons = response.data.results;
         })
         .catch(error => {
-          console.log('-----error-------');
+          console.log("-----error-------");
           console.log(error);
-        })
+        });
+    },
+    setToTop() {
+      // alert('tes')
+      setTimeout(function(){
+      document.body.scrollTop = 0; // For Safari
+      document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+      }, 100);
     }
   }
-}
+};
 </script>
--- src/PokemonDetail.vue
+++ src/PokemonDetail.vue
@@ -1,13 +1,17 @@
 <template lang="html">
+
   <div class="post" v-if="pokemon">
-    <h1 class="post__title icon-pokemon"><img id="emotion" :src="pokemon.sprites['front_default']" @click="tangkap(pokemon)"/> {{ pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1) }}</h1>
+    <h1 class="post__title icon-pokemon">{{ pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1) }}</h1>
     <p class="post__body">
+      <img id="emotion" :src="pokemon.sprites['front_default']" @click="tangkap(pokemon)" width="120" height="120" />
       Another form (click to flip):
       <div class="row">
+
   <div class="column-sprites" v-if="value" v-for="(value,propertyName) in pokemon.sprites">
   <img class="flipthis" :src="value" :title="propertyName" />
 </div>
 </div>
+
 Height: {{ pokemon.height }}<br>
 Weight: {{ pokemon.weight }}<br>
 Abilities:
@@ -37,16 +41,20 @@ Moves:
     </p>
     <p class="post__id"></p>
   </div>
+   <div class="post" v-else>
+<center><img src="assets/pocket.png" class="pocket-bounces" />
+<h4>Please wait....</h4></center>
+</div>
 </template>
 <style>
 @font-face {
-font-family: "Pokemon";
-src: url("/assets/Pokemon_Hollow.ttf") format("truetype");
+  font-family: "Pokemon";
+  src: url("/assets/Pokemon_Hollow.ttf") format("truetype");
 }
 h1.icon-pokemon {
-  font-family: 'Pokemon';
-  font-weight:normal;
-  font-style:normal;
+  font-family: "Pokemon";
+  font-weight: normal;
+  font-style: normal;
 }
 img.flipthis:active {
   -webkit-transform: scaleX(-1);
@@ -56,61 +64,62 @@ img {
   vertical-align: middle;
 }
 img.emotion:hover {
-  src: url('/assets/pokeball.png') no-repeat;
+  src: url("/assets/pokeball.png") no-repeat;
 }
 </style>
 <script>
-import axios from 'axios';
+import axios from "axios";
 export default {
-  props: ['name'],
+  props: ["name"],
   data() {
     return {
       pokemon: null,
       speed: 0,
       sprites: [],
-      endpoint: 'https://pokeapi.co/api/v2/pokemon/',
-    }
+      endpoint: "https://pokeapi.co/api/v2/pokemon/"
+    };
   },
   watch: {
-  '$route'() {
-    this.getPokemon(this.name);
-  }
-},
+    $route() {
+      this.getPokemon(this.name);
+    }
+  },
   methods: {
     tangkap(fokemon) {
-    // alert('tes');
-    console.log(fokemon);
+      // alert('tes');
+      console.log(fokemon);
     },
-  getPokemon(name) {
-    axios(this.endpoint + name)
-      .then(response => {
-        this.pokemon = null
-        this.speed  = parseInt(response.data.stats[0].base_stat) * 10
-        this.sprites  = response.data.sprites
-        this.pokemon = response.data
-      })
-      .catch( error => {
-        console.log(error)
-      })
+    getPokemon(name) {
+      this.pokemon = null;
+      axios(this.endpoint + name)
+        .then(response => {
+          this.speed = 0;
+          this.speed = parseInt(response.data.stats[0].base_stat) * 10;
+          this.sprites = response.data.sprites;
+          this.pokemon = response.data;
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  },
+
+  created() {
+    this.getPokemon(this.name);
+  },
+
+  mounted() {
+    const self = this;
+    setTimeout(function() {
+      setInterval(function() {
+        var x = Math.floor(Math.random() * self.speed);
+        var y = Math.floor(Math.random() * self.speed);
+        var obj = document.getElementById("emotion");
+        obj.style.position = "absolute";
+        obj.style.top = x + "px";
+        obj.style.left = y + "px";
+      }, self.speed);
+    }, 1000);
   }
-},
-
-created() {
-  this.getPokemon(this.name);
-},
-
-mounted() {
-      const self = this
- setTimeout(function(){
- setInterval(function(){
-  var x = Math.floor(Math.random() * self.speed);
-var y = Math.floor(Math.random() * self.speed);
-var obj = document.getElementById("emotion");
-obj.style.position = 'absolute';
-obj.style.top = x + "px";
-obj.style.left = y + "px";
- }, self.speed);
-  }, 1000);
-}
-}
+};
 </script>
